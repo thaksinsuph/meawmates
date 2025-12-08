c4e0b90a7ee90bcff00a03e0b22dd73b029c1256
hanggybird <thaksin.suph@bumail.net>
2025-12-08 16:41:38 +0700

    update...

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,7 @@ const io = new Server(server, {
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     },
     // 🎯 1. แก้ไข WebSocket Error: กำหนด Path และ Transports สำหรับ Production
-    path: '/socket.io/', // ยืนยัน Path
+    path: '/socket.io', // ยืนยัน Path
     transports: ['websocket', 'polling'], // บังคับให้ใช้ WebSocket
 });
 
