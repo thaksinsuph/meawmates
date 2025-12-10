4262e95abf8cf4718add1b7b1cdc4080a0f11726
hanggybird <thaksin.suph@bumail.net>
2025-12-10 13:23:16 +0700

    update...

--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -82,7 +82,7 @@ const CatProfileModal = ({ modalState, onClose, onSelectCat }) => {
                                 </button>
                             ))}
                         </div>
-                    )}
+                    )}  
                     
                     <img
                         src={cat.image}
